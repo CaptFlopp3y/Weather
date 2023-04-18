241dd2d88f8d6949145c631a263ee35c8fbc50bb
CaptainFlopp3y <barrancofabian@gmail.com>
2023-04-18 11:30:53 -0600

    head hurts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var apiurl = "https://api.openweathermap.org/data/2.5/weather?q=salt lake city&appid=55005b98484cc48d65537d2a2b526975"
+var apiurl2 = "https://api.openweathermap.org/data/2.5/onecall?appid=16da2c71dd8c2c76dfce15f0f75a5dea&lat=40.7608&lon=-111.8911"
 var listEl = document.getElementById("myData")
+var listE2 = document.getElementById("forecast")
+
 
 var response = fetch(apiurl)
     .then(function (response) {
@@ -7,17 +10,29 @@ var response = fetch(apiurl)
     })
 
     .then(function (data) {
-        // var docArray = data.list;
-        // for (var i = 0; i < docArray.length; i++) {
-        //     var listItem = document.createElement(`li`);
-        //     listItem.textContent = docArray[i].main;
-        //     var weatherRow = document.createElement("div");
-        //     weatherRow.innerHTML = `
-        //         <p class="col-sm"> Temp: ${docArray[i].main.temp} </p>
-        //             `;
+        const event = new Date();
+        var date = event.getMonth() + ", " + event.getDate() + ", " + event.getFullYear()
+        var lon = data.coord.lon
+        var lat = data.coord.lat
+        var pic = data.weather[0].icon
+        var temp = data.main.temp
+        var wind = data.wind.speed
+        var hum = data.main.humidity
+        var city = data.name
+        var html = `
+        <div class= "col-12">
+        <p>${city}, ${date} </p>
+        <p>
+        <img src="http://openweathermap.org/img/wn/${pic}.png"
+        alt= "Weather Icon" />
+        </p>
+        <p>Temp: ${temp} </p>
+        <p>Wind: ${wind} MPH</p>
+        <p>Humidity: ${hum} %</p>
+        </div>
+        `
 
-        //     listEl.appendChild(listItem);
-        // }
+        listEl.innerHTML = html
         console.log(data);
     })
 
@@ -25,3 +40,7 @@ var response = fetch(apiurl)
         console.log(error)
     });
 
+var response = fetch(apiurl2)
+    .then(function (response) {
+        return response.json();
+    })
